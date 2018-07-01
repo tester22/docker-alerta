70e4b1db16122de0165a00b202561b156b4bcb4b
Anton <anton@trugen.net>
2018-07-01 22:30:22 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,12 +7,9 @@ RUN_ONCE=/app/.run_once
 # MongoDB configuration / preparation
 # ---------------------------------------------------------------------------
 # Make sure MongoDB directory exists.
-# Make sure MongoDB directory exists.
-if [ ! -d "${MONGODB_DATA}" ]; then
-    mkdir "${MONGODB_DATA}" || exit $?
-    chown "${MONGODB_USER}:${MONGODB_GROUP}" "${MONGODB_DATA}" || exit $?
-fi
-
+mdkir /var/local
+mkdir "${MONGODB_DATA}"
+chown "${MONGODB_USER}:${MONGODB_GROUP}" "${MONGODB_DATA}"
 
 # Generate web console config, if not supplied
 if [ ! -f "${ALERTA_WEB_CONF_FILE}" ]; then
